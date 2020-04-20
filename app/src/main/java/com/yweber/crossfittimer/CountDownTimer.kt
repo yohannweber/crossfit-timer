8febc3d72867588675df05eebbbcc7d2b7a7883a
YohannWEBER <yohann.weber@gmail.com>
2020-04-20 22:22:44 +0200

    gestion des resizing à revoir

--- app/src/main/java/com/yweber/crossfittimer/CountDownTimer.kt
+++ app/src/main/java/com/yweber/crossfittimer/CountDownTimer.kt
@@ -8,29 +8,24 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import kotlin.math.min
 
-class CountDownTimer(millisInFuture: Long = 60_000, private val images : Array<ImageView>, baseContext: Context) : CountDownTimer(millisInFuture, 1000) {
-    init{
-        loadDrawables(baseContext)
-    }
+class CountDownTimer(millisInFuture: Long = 60_000, private val images : Array<ImageView?>, baseContext: Context) : CountDownTimer(millisInFuture, 500) {
 
-    lateinit var drawables : Array<Drawable?>
-    var pointsPersistency : Long = 0
+    var drawables : Array<Drawable?> = arrayOf(
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_zero),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_one),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_two),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_three),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_four),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_five),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_six),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_seven),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_eigth),
+        ContextCompat.getDrawable(baseContext, R.drawable.ic_nine))
 
-    private fun loadDrawables(baseContext: Context) {
-        drawables = arrayOf(
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_zero),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_one),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_two),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_three),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_four),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_five),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_six),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_seven),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_eigth),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_nine),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_point),
-            ContextCompat.getDrawable(baseContext, R.drawable.ic_no_point_))
-    }
+    val drawablePoint =    ContextCompat.getDrawable(baseContext, R.drawable.ic_point)
+    val drawableNoPoint = ContextCompat.getDrawable(baseContext, R.drawable.ic_no_point_)
+    val drawableD = ContextCompat.getDrawable(baseContext, R.drawable.ic_d)
+    val drawableN = ContextCompat.getDrawable(baseContext, R.drawable.ic_n)
 
     override fun onFinish() {
     }
@@ -41,27 +36,22 @@ class CountDownTimer(millisInFuture: Long = 60_000, private val images : Array<I
 
     private fun updateTimer(millisUntilFinished: Long) {
         val minutes : Int = (millisUntilFinished / 60_000).toInt()
-        val seconds : Int = ((millisUntilFinished - minutes * 60_000 )/ 1000).toInt()
-
-        if(millisUntilFinished.rem(1000) == 0.toLong())
-            pointsPersistency = 0
-        else
-            pointsPersistency += 1
+        val seconds : Int = ((millisUntilFinished - minutes * 60_000 )/ 1_000).toInt()
 
 
         val first = (minutes / 10)
         val second = minutes.rem(10)
         val third = (seconds / 10)
         val fourth = seconds.rem(10)
-        images[0].setImageDrawable( getImage(first))
-        images[1].setImageDrawable( getImage(second))
-        images[2].setImageDrawable( getImage(third) )
-        images[3].setImageDrawable( getImage(fourth))
+        images[0]?.setImageDrawable( getImage(first))
+        images[1]?.setImageDrawable( getImage(second))
+        images[2]?.setImageDrawable( getImage(third) )
+        images[3]?.setImageDrawable( getImage(fourth))
 
         if(millisUntilFinished.rem(1000) in 0..600)
-            images[4].setImageDrawable( getImage(11))
+            images[4]?.setImageDrawable( drawableNoPoint)
         else
-            images[4].setImageDrawable( getImage(10))
+            images[4]?.setImageDrawable( drawablePoint)
 
     }
 
--- app/src/main/java/com/yweber/crossfittimer/MainActivity.kt
+++ app/src/main/java/com/yweber/crossfittimer/MainActivity.kt
@@ -2,6 +2,9 @@ package com.yweber.crossfittimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.WindowManager
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        countDownTimer = CountDownTimer(60_000, arrayOf(iv_first, iv_second, iv_third, iv_fourth, iv_points), baseContext)
+
+        // get device dimensions
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        var width = displayMetrics.widthPixels / displayMetrics.density
+        var height = displayMetrics.heightPixels /displayMetrics.density
+
+        resizeImageView(displayMetrics, arrayOf(iv_first, iv_second, iv_third, iv_fourth, iv_points, iv_points, iv_prefix1,iv_prefix2))
+
+        //textV.text = width.toString() + " x " +height.toString()
+        countDownTimer = CountDownTimer(61_000, arrayOf(iv_first, iv_second, iv_third, iv_fourth, iv_points), baseContext)
         timerButton.setOnClickListener {
             if (!timerIsRunning)
                 countDownTimer.start()
@@ -21,6 +35,22 @@ class MainActivity : AppCompatActivity() {
 
             timerIsRunning = !timerIsRunning
 
+        }
+    }
+
+    fun resizeImageView( displayMetrics: DisplayMetrics, imagesView : Array<ImageView?>){
+        var width = displayMetrics.widthPixels / 7
+        var layoutParams = iv_first.layoutParams
+
+        val sizingCoef = width / layoutParams.width
+        val newWidth = width
+        val newHeight = layoutParams.height * sizingCoef
+
+        for (imageView in imagesView){
+            layoutParams = imageView?.layoutParams
+            layoutParams.width = newWidth.toInt()
+            layoutParams.height = newHeight.toInt()
+            imageView?.layoutParams= layoutParams
         }
     }
 
